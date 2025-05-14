76b3810002bf500796ba790c3ddabecb26ab5e89
Josh Phillips <joshphillips@Joshs-MacBook-Pro-2.local>
2025-05-14 00:09:22 -0400

    comments in block number compression

--- src/transfers/compression/block_number.rs
+++ src/transfers/compression/block_number.rs
@@ -19,19 +19,24 @@ impl RLECompressedBlockNumberSeries {
     pub fn compress_block_number(&mut self, dataset: &DataFrame) -> Result<(Vec<u32>, Vec<u16>), Box<dyn std::error::Error>> {
         // establish incoming col len // let num_rows = dataset.height();
 
+        // Distill block_number column from incoming dataset and 
+        // convert to vec of type u32
         let blocks = dataset.column("block_number").unwrap();
         let block_vec: Vec<Option<u32>> = blocks.u32()?.into_iter().collect();
 
-        // early return if vec is empty
+        // Return empty tuple of vec if empty
         if block_vec.is_empty() {
             return Ok((Vec::new(), Vec::new()));
         }
 
-       // set initial block as current value, and initial count as 1
+        // Begin iteration setup by starting with the
+        // first value of the vector, and set the count
+        // to 1.
         let mut current_value = block_vec[0].unwrap();
         let mut current_count = 1 as u16;
 
-        // iterate through blocks, skip first, 
+        // Iterate through block_vec, skipping the first
+        // since that is set as current_value
         for block in block_vec.iter().skip(1) {
             let b = block.unwrap();
             if b == current_value {
@@ -47,11 +52,12 @@ impl RLECompressedBlockNumberSeries {
         self.values.push(current_value);
         self.counts.push(current_count.into());
 
-        // check size comparisons
+        // Check size comparisons
         let block_size = block_vec.capacity() * mem::size_of::<Option<u32>>();
         let compressed_size = self.values.capacity() * mem::size_of::<u32>() + 
                             self.counts.capacity() * mem::size_of::<u16>();
 
+        // Optional output print statements for comparison
         println!("Original block index: {} bytes", block_size.red());
         println!("Compressed block index: {} bytes", compressed_size.green());
         println!("Compression Ratio: {:.2}", block_size as f64 / compressed_size as f64);
